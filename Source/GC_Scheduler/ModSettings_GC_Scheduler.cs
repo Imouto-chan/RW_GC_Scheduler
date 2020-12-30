4b0fa61d65cbf317d59c3e75558a1b217b310a25
Imouto-chan <76026763+Imouto-chan@users.noreply.github.com>
2020-12-29 20:09:39 -0500

    Modified UI of settings page, added offset parameters, fixed UI colors
    - Modified mod settings page so that everything is placed correctly
    - Added offset parameters so the UI can be shifted around depending on what calls it's function
    - Fixed UI color issues where it was spreading past the point it was supposed to

--- Source/GC_Scheduler/ModSettings_GC_Scheduler.cs
+++ Source/GC_Scheduler/ModSettings_GC_Scheduler.cs
@@ -36,15 +36,17 @@ namespace GC_Scheduler
             listing.Begin(rect);
             Text.Font = GameFont.Small; // Writes following labels in small font
             listing.CheckboxLabeled("ShowMainTab".Translate(), ref showMainTab); // check box to decide whether or not to show the mod tab in-game
+            listing.Gap(5f);
+            listing.Label("lblStatsNote".Translate());
             listing.End();
 
-            Rect section2 = new Rect((rect.x + rect.width) / 2 - rect.width / 2, rect.y + listing.CurHeight, rect.width, 450f);
-            DoInGameSettingsWindwo(section2);
+            Rect section2 = new Rect(rect.x, rect.y + 20f, rect.width, 450f);
+            DoInGameSettingsWindwo(section2, sectionOffsetY: -55f );
 
             GUI.EndGroup();
         }
 
-        public void DoInGameSettingsWindwo(Rect rect)
+        public void DoInGameSettingsWindwo(Rect rect, float sectionOffsetX = 0f, float sectionOffsetY = 0f)
         {
             GUI.BeginGroup(rect);
 
@@ -56,41 +58,46 @@ namespace GC_Scheduler
 
             if (UnityEngine.Scripting.GarbageCollector.GCMode == UnityEngine.Scripting.GarbageCollector.Mode.Enabled) // Display status to show if GC is enabled or disabled
             {
-                listing.Label("GCStatusEnabled".Translate());
+                listing.Label("lblGCStatusEnabled".Translate());
             }
             else
             {
-                listing.Label("GCStatusDisabled".Translate());
+                listing.Label("lblGCStatusDisabled".Translate());
             }
 
             listing.CheckboxLabeled("enableGCScheduler".Translate(), ref gcScheduler); // Display button whether to enable the GC Scheduler or not
 
             if (gcScheduler)
             {
-                listing.Gap(5f); // Display current update tick the game is on
+                listing.GapLine(5f);
+                // Display current update tick the game is on
                 listing.Label("lblCurrentTickInterval".Translate() + ": " + MainButtonWorker_GC_Scheduler.updateTick + " / " + updateInterval);
-                listing.Gap(5f); // Display current memory usage
+                listing.Gap(5f); 
+                // Display current memory usage
                 listing.Label("lblCurrentMemoryUsage".Translate() + ": \n" + MainButtonWorker_GC_Scheduler.currentMemory + " MB / " + totalMemory + " MB");
-                listing.Gap(5f); // Display option whether or not to force pause the game when the menu is opened
+                listing.GapLine(5f); 
+                // Display option whether or not to force pause the game when the menu is opened
                 listing.CheckboxLabeled("lblForcePause".Translate(), ref forcePause, "lblForcePauseDescription".Translate());
                 listing.Gap(5f);
 
                 listing.Label("lblSetTickInterval".Translate() + ": " + updateInterval + " " + "lblTicks".Translate(), tooltip: "lblSetTickIntervalDescription".Translate());
-                Rect section2 = new Rect(rect.x, rect.y + listing.CurHeight + 10f, rect.width, 25f); // Bar to set amount of ticks to pass before updating memory
+                Rect section2 = new Rect(rect.x + sectionOffsetX, rect.y + listing.CurHeight + 10f + sectionOffsetY, rect.width, 25f); 
+                // Bar to set amount of ticks to pass before updating memory
                 updateInterval = (int)Widgets.HorizontalSlider(section2, updateInterval, 300f, 3600f, leftAlignedLabel: "300", rightAlignedLabel: "3600");
 
                 listing.Gap(40f);
                 listing.Label("lblMinGCMemory".Translate() + ": " + totalMemory.ToString() + " MB", tooltip: "lblMinGCMemoryDescription".Translate());
-                Rect section3 = new Rect(rect.x, rect.y + listing.CurHeight + 10f, rect.width, 25f); // Bar to set total memory to keep track of and minimum memory before using GC
+                Rect section3 = new Rect(rect.x + sectionOffsetX, rect.y + listing.CurHeight + 10f + sectionOffsetY, rect.width, 25f);
+                // Bar to set total memory to keep track of and minimum memory before using GC
                 totalMemory = Widgets.HorizontalSlider(section3, totalMemory,
-                                                                                    1024f, Math.Max((float)SystemInfo.systemMemorySize - 4096f, 2048f),
-                                                                                    leftAlignedLabel: "1024 MB", rightAlignedLabel: Math.Max((float)SystemInfo.systemMemorySize - 4096f, 2048f).ToString() + " MB");
+                                                        1024f, Math.Max((float)SystemInfo.systemMemorySize - 4096f, 2048f),
+                                                        leftAlignedLabel: "1024 MB", rightAlignedLabel: Math.Max((float)SystemInfo.systemMemorySize - 4096f, 2048f).ToString() + " MB");
 
-                Rect section4 = new Rect(rect.x, rect.y + section3.y + section3.height + 5f, rect.width, 50f);
+                Rect section4 = new Rect(rect.x + sectionOffsetX, rect.y + section3.y + section3.height + 5f + sectionOffsetY, rect.width, 50f);
                 List<ListableOption> list = new List<ListableOption>();
                 if (UnityEngine.Scripting.GarbageCollector.GCMode == UnityEngine.Scripting.GarbageCollector.Mode.Enabled) // If GC is enabled, show disable button and vice versa
                 {
-                    GUI.color = new Color(0.85f, 0f, 0f); // Sets color to red
+                    GUI.color = Color.red; // Sets color to red so proceeding GUI is in red
                     list.Add(new ListableOption("BtnStopGC".Translate(), delegate () // disable GC button
                     {
                         UnityEngine.Scripting.GarbageCollector.GCMode = UnityEngine.Scripting.GarbageCollector.Mode.Disabled;
@@ -99,7 +106,7 @@ namespace GC_Scheduler
                 }
                 else
                 {
-                    GUI.color = new Color(0.4f, 85f, 0f); // Sets color to green
+                    GUI.color = Color.green; // Sets color to red so proceeding GUI is in green
                     list.Add(new ListableOption("BtnStartGC".Translate(), delegate () // enable GC button
                     {
                         UnityEngine.Scripting.GarbageCollector.GCMode = UnityEngine.Scripting.GarbageCollector.Mode.Enabled;
@@ -109,6 +116,7 @@ namespace GC_Scheduler
                 OptionListingUtility.DrawOptionListing(section4, list); // Adds buttons to section4
             }
 
+            GUI.color = Color.white;
             listing.End();
             GUI.EndGroup();
         }
